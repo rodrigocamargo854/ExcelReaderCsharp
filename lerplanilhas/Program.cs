06492b287a9c37dc880072e2abc0db26cfb34460
Rodrigo Correa de Godoy Camargo <rodrigo.godoy@hbsis.com.br>
2021-03-30 06:57:48 -0300

    First commit

---
+++ lerplanilhas/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using ClosedXML.Excel;
+
+namespace lerplanilhas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var xls = new XLWorkbook(@"C:\Temp\ExemploExcel.xlsx");
+           var planilha = xls.Worksheets.First(w => w.Name == "Planilha1");
+            var totalLinhas = planilha.Rows().Count();
+            // primeira linha é o cabecalho
+          
+        }
+    }
+}
